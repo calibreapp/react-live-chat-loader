8d976228bbdf926a415067f352cfd2303c76d822
Michael Dijkstra <micdijkstra@gmail.com>
2020-03-23 14:29:12 +1100

    Baseline/noscript page

---
+++ website/pages/noscript.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../layouts/main'
+
+const Page = () => (
+  <Layout title="Baseline">
+    <div className="wrapper">
+      <div className="inner">
+        <h1>Baseline</h1>
+        <p>
+          Baseline comparison page for React Live Chat Loader with no script
+          included.
+        </p>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default Page
