681e54512b48af08ca2039a1a13d3c871a4ce762
Michael Dijkstra <micdijkstra@gmail.com>
2020-03-30 09:35:54 +1100

    Fix Messenger Loading (#23)
    * Fix Messenger rendering
    
    - Ensure CustomerChat is rendered once and stays rendered
    - Remove attribution
    
    * Messenger: Add 1 to z-index so it loads underneath
    
    * Show Messenger on click

--- src/providers/messenger.js
+++ src/providers/messenger.js
@@ -37,7 +37,7 @@ const load = ({ appID, locale = 'en_US' }) => {
 }
 
 const open = () => {
-  window.FB.CustomerChat.show(false)
+  window.FB.CustomerChat.show(true)
 }
 const close = () => {
   window.FB.CustomerChat.hide()
