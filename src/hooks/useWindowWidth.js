2452d96566e640668a0487d81c261156bf9378fc
Jeff Reiner <theundeadghost@gmail.com>
2020-04-11 12:48:02 +0200

    Adding useWindowWidth hook

---
+++ src/hooks/useWindowWidth.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth] = useState(
+    typeof window !== 'undefined' ? window.innerWidth : 0
+  )
+
+  const handleResize = () => setWindowWidth(window.innerWidth)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setWindowWidth(window.innerWidth)
+      window.addEventListener('resize', handleResize)
+    }
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', handleResize)
+      }
+    }
+  }, [])
+
+  return windowWidth
+}
+
+export default useWindowWidth
