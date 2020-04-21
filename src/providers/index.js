63b4cc8dc8e841e5c3bb392319fa3bb1d9b5c247
Jeff Reiner <theundeadghost@gmail.com>
2020-04-21 02:01:37 +0200

    Adding new Drift chat provider and component (#7)
    * Adding new Drift chat provider and component
    
    * Increasing zIndex
    
    * Adding docs for Drift messenger
    
    * Addressing code-review and linter errors
    
    * Removing isRequired prop per review
    
    * Adding useWindowWidth hook
    
    * Fixing issue with spacing of the element on diff devices

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,8 +1,10 @@
+import drift from './drift'
 import helpScout from './helpScout'
 import intercom from './intercom'
 import messenger from './messenger'
 
 const Providers = {
+  drift,
   helpScout,
   intercom,
   messenger
