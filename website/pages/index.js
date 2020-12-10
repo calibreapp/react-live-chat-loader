281292e35978c383d785d4a735edf4b1ed6f2486
Ben Schwarz <ben@calibreapp.com>
2020-12-10 17:17:33 +1100

    Rearrange the page so that demos are clearly shown

--- website/pages/index.js
+++ website/pages/index.js
@@ -11,23 +11,7 @@ const Page = () => (
           An npm module that allows you to mitigate the negative performance and
           user experience impact of chat tools. React-live-chat-loader shows a
           fake widget until the page has finished loading or users are ready to
-          interact with chat. Currently works with{' '}
-          <Link href="/intercom">
-            <a>Intercom</a>
-          </Link>
-          ,{' '}
-          <Link href="/helpscout">
-            <a>Help Scout</a>
-          </Link>
-          ,{' '}
-          <Link href="/drift">
-            <a>Drift</a>
-          </Link>
-          , and{' '}
-          <Link href="/messenger">
-            <a>Facebook Messenger</a>
-          </Link>
-          .
+          interact with chat.
         </p>
         <p>
           <a href="https://github.com/calibreapp/react-live-chat-loader">
@@ -36,8 +20,33 @@ const Page = () => (
         </p>
         <p>
           <a href="https://calibreapp.com">
-            Need web performance monitoring? <span>→</span>
+            Built by the team at Calibre <span>→</span>
           </a>
+        </p>
+        <p>
+          View demos:{' '}
+          <ul>
+            <li>
+              <Link href="/intercom">
+                <a>Intercom</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/helpscout">
+                <a>Help Scout</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/drift">
+                <a>Drift</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/messenger">
+                <a>Facebook Messenger</a>
+              </Link>
+            </li>
+          </ul>
         </p>
       </div>
     </div>
