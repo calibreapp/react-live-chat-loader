ee239482c9cb336e320772fda8e47deef6e32a36
♗ nathan <elmoeleven@gmail.com>
2021-03-14 20:58:20 -0400

    Move index from js to ts.

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { default as useChat } from './hooks/useChat'
+import { default as useProvider } from './hooks/useProvider'
+import { default as Drift } from './components/Drift'
+import { default as HelpScout } from './components/HelpScout'
+import { default as Intercom } from './components/Intercom'
+import { default as Messenger } from './components/Messenger'
+import { default as LiveChatLoaderProvider } from './components/LiveChatLoaderProvider'
+
+export {
+  useChat,
+  useProvider,
+  Drift,
+  HelpScout,
+  Intercom,
+  Messenger,
+  LiveChatLoaderProvider
+}
