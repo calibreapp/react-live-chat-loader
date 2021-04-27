870220779e8c94b42be66c165ca418097108d73e
Michael Dijkstra <micdijkstra@gmail.com>
2021-04-27 16:33:07 +1000

    Import and Export Userlike

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { default as Drift } from './components/Drift'
 import { default as HelpScout } from './components/HelpScout'
 import { default as Intercom } from './components/Intercom'
 import { default as Messenger } from './components/Messenger'
-export { default as Userlike } from './components/Userlike'
+import { default as Userlike } from './components/Userlike'
 import { default as LiveChatLoaderProvider } from './components/LiveChatLoaderProvider'
 
 export {
@@ -14,5 +14,6 @@ export {
   HelpScout,
   Intercom,
   Messenger,
+  Userlike,
   LiveChatLoaderProvider
 }
