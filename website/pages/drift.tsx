2c00fb293c1d3a75d73f73dca9e17291d12eced9
Rob Morieson <rob@electricanimals.com>
2022-10-06 16:40:59 +1100

    Ensure <ul> is not child of <p>

--- website/pages/drift.tsx
+++ website/pages/drift.tsx
@@ -18,9 +18,8 @@ const Page: NextPage = () => (
             </a>
             .
           </p>
-          <p>
-            View other demos: <ExampleLinks />
-          </p>
+          <p>View other demos:</p>
+          <ExampleLinks />
         </div>
       </div>
       <Drift icon="A" color="#0176ff" />
